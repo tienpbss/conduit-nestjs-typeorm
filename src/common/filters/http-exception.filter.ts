2bdba2c92d4a14ca16fc5a432af9545581a353ce
tienpbss <tienphung.cn07@gmail.com>
2023-10-11 14:48:42 +0700

    add exception filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,39 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    let bodyError: string | object;
+    const exceptionRes: any = exception.getResponse();
+    if (typeof exceptionRes === 'object') {
+      bodyError = exceptionRes.message ?? exceptionRes;
+    } else {
+      bodyError = exceptionRes;
+    }
+
+    const errors = {
+      statusCode: status,
+      error: exception.message,
+      body: bodyError,
+    };
+
+    response.status(status).json(errors);
+  }
+}
+// {
+//   "errors":{
+//     "body": [
+//       "can't be empty"
+//     ]
+//   }
+// }
